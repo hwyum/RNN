492b792d4d04c67a7f2172d1e068525f852794c5
Kion Kim <kion.kim@gmail.com>
2018-10-25 23:34:12 +0900

    modified after the presentation

--- python/4_sentiment_LSTM_Cell.py
+++ python/4_sentiment_LSTM_Cell.py
@@ -9,26 +9,6 @@ from mxnet.gluon import nn, rnn
 from utils import *
 os.environ['MXNET_ENGINE_TYPE'] = 'NaiveEngine'
 
-class Sentence_Representation(nn.Block): ## Using LSTM
-    def __init__(self, emb_dim, hidden_dim, vocab_size, dropout = .2, **kwargs):
-        super(Sentence_Representation, self).__init__(**kwargs)
-        self.vocab_size = vocab_size
-        self.emb_dim = emb_dim
-        self.hidden_dim = hidden_dim
-        with self.name_scope():
-            self.hidden = []
-            self.embed = nn.Embedding(vocab_size, emb_dim)
-            self.lstm = rnn.LSTM(hidden_dim // 2, dropout = dropout \
-                               , input_size = emb_dim \
-                               , bidirectional = True \
-                               , layout = 'NTC')
-
-    def forward(self, x, hidden):
-        embeds = self.embed(x) # batch * time step * embedding: NTC
-        lstm_out, self.hidden = self.lstm(embeds, hidden)
-        # (2 * num_layers) * Batch size * Hidden dim 
-        return lstm_out, self.hidden
-
 class Sentence_Representation(nn.Block): ## Using LSTMCell : Only use the last time step
     def __init__(self, emb_dim, hidden_dim, vocab_size, dropout = .2, **kwargs):
         super(Sentence_Representation, self).__init__(**kwargs)
@@ -40,23 +20,24 @@ class Sentence_Representation(nn.Block): ## Using LSTMCell : Only use the last t
             self.b_hidden = []
             self.embed = nn.Embedding(self.vocab_size, self.emb_dim)
             self.drop = nn.Dropout(.2)
-            self.f_lstm = rnn.LSTMCell(self.hidden_dim // 2)
-            self.b_lstm = rnn.LSTMCell(self.hidden_dim // 2)
+            self.bi_rnn = rnn.BidirectionalCell(
+                rnn.LSTMCell(hidden_size = self.hidden_dim // 2),
+                rnn.LSTMCell(hidden_size = self.hidden_dim // 2)
+            )
             
-    def forward(self, x, _f_hidden, _b_hidden):
+    def forward(self, x, _f_hidden):
         embeds = self.embed(x) # batch * time step * embedding
-        self.f_hidden = _f_hidden
-        self.b_hidden = _b_hidden
-        # Forward LSTM
-        for i in range(embeds.shape[1]):
-            dat = embeds[:, i, :]
-            _, self.f_hidden = self.f_lstm(dat, self.f_hidden)
-        # Backward LSTM
-        for i in np.arange(embeds.shape[1], 0, -1):
-            dat = embeds[:, np.int(i - 1), :] # np.int() necessary
-            _, self.b_hidden = self.b_lstm(dat, self.b_hidden)
-        x = nd.concat(self.f_hidden[0], self.b_hidden[0], dim = 1)
-        return x
+        _, h = self.bi_rnn.unroll(length = embeds.shape[1] \
+                                , inputs = embeds \
+                                , layout = 'NTC' \
+                                , merge_outputs = True)
+        #print('h shape = {}'.format(nd.concat(h[1], h[3], dim = 1).shape))
+        return nd.concat(h[1], h[3], dim = 1)
+
+    
+    def begin_state(self, *args, **kwargs):
+        return self.bi_rnn.begin_state(*args, **kwargs)
+    
     
 class SA_Classifier(nn.Block):
     def __init__(self, sen_rep, classifier, batch_size, context, **kwargs):
@@ -68,17 +49,14 @@ class SA_Classifier(nn.Block):
             self.classifier = classifier
             
     def forward(self, x):
-        b_hidden = self.sen_rep.b_lstm.begin_state(func = mx.nd.zeros \
-                                               , batch_size = self.batch_size \
-                                               , ctx = self.context)
-        f_hidden = self.sen_rep.f_lstm.begin_state(func = mx.nd.zeros \
+        hidden = self.sen_rep.begin_state(func = mx.nd.zeros \
                                                , batch_size = self.batch_size \
                                                , ctx = self.context)
 
-        _x = self.sen_rep(x, f_hidden, b_hidden) # Use the last hidden step
+        _x = self.sen_rep(x, hidden) # Use the last hidden step
         # Extract hidden state from _x
-        x = self.classifier(_x)
-        return x           
+        _x = self.classifier(_x)
+        return _x           
     
     
 if __name__ == '__main__':
@@ -103,16 +81,16 @@ if __name__ == '__main__':
     train_data = mx.io.NDArrayIter(data=[tr_x, tr_y], batch_size=batch_size, shuffle = False)
     valid_data = mx.io.NDArrayIter(data=[va_x, va_y], batch_size=batch_size, shuffle = False)
 
+    # Sentence representation
+    sen_rep = Sentence_Representation(emb_dim, hidden_dim, MAX_VOCAB)
     # Classifier
     classifier = nn.Sequential()
     classifier.add(nn.Dense(16, activation = 'relu'))
     classifier.add(nn.Dense(8, activation = 'relu'))
     classifier.add(nn.Dense(1))
-    classifier.collect_params().initialize(mx.init.Xavier(), ctx = context)
-
-    # Sentence representation
-    sen_rep = Sentence_Representation(emb_dim, hidden_dim, max_vocab, dropout = .5)
-    sen_rep.collect_params().initialize(mx.init.Xavier(), ctx = context)
+    sa = SA_Classifier(sen_rep, classifier, 2, context)
+    sa.collect_params().initialize(mx.init.Xavier(), ctx = context)
+    trainer = gluon.Trainer(sa.collect_params(), 'adam', {'learning_rate': 1e-3})
 
     # Classifier
     sa = SA_Classifier(sen_rep, classifier,  batch_size, context)
